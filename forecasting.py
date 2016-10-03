22b8bf45b10e5b39ab09d4a41f1388821c269f86
Clark, Peter <peter.clark@ft.com>
2016-10-03 15:48:52 +0100

    Adding lunch and forecasting wip

---
+++ forecasting.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python
+# coding = utf-8
+
+import click
+import ledger
+
+@click.command()
+@click.argument('query')
+def run(query):
+    journal = ledger.read_journal("./secret/ledger.dat")
+    for xact in journal.xacts():
+        for post in xact.posts():
+            print post.date, post.account, post.amount
+            
+    for xact in ledger.read_journal("./secret/bills.dat").xacts():
+        for post in xact.posts():
+            print post.date, post.account, post.amount
+
+if __name__ == '__main__':
+    run()
